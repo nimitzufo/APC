05ae0771216ba40546b327bdc8ddfc3cc8ff386f
nimitzufo <nimitzufo@protonmail.com>
2019-10-20 20:41:23 -0300

    nothing much

--- alturas.c
+++ alturas.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+
 /*Síntese
-Objetivo: obter maior e menor altura, média das alturas das mulheres e o nro de homens.
+Objetivo: obter maior e menor altura, média das alturas das mulheres e
+o nro de homens.
 Entrada: strings(masculino, feminino), alturas de 10 pessoas.
 Saída: maior e menor altura, media das alturas(mulheres), nro de homens
 */
- 
+
 typedef struct pessoa{
   char sexo[10];
   float altura;
 } PESSOA;
- 
+
 void menAltura(PESSOA *vPessoa, float *menorAlt){
     int aux = 0;
     float tmp = vPessoa[0].altura;
@@ -30,8 +31,8 @@ void maiAltura(PESSOA *vPessoa, float *maiorAlt){
             tmp=vPessoa[aux+1].altura;
     }
     *maiorAlt = tmp;
-}  
- 
+}
+
 void mediaAltMulheres(PESSOA *vPessoa, int nroMulheres, float *mediaAltFem){
     int aux=0;
     float soma = 0.00;
@@ -41,12 +42,12 @@ void mediaAltMulheres(PESSOA *vPessoa, int nroMulheres, float *mediaAltFem){
     }
     *mediaAltFem = soma/nroMulheres;
 }
- 
+
  main(){
     PESSOA vPessoa[10];
     float menorAlt, maiorAlt, mediaAltFem;
     int nroHomens=0, nroMulheres=0, aux=0;
-   
+
     for(aux=0;aux<10;aux++){
         printf("Registro nro %d\n", aux);
         printf("Informe a altura: ");
@@ -60,12 +61,12 @@ void mediaAltMulheres(PESSOA *vPessoa, int nroMulheres, float *mediaAltFem){
         	nroMulheres++;
         if(strcmp(vPessoa[aux].sexo, "masculino")!=0&&strcmp(vPessoa[aux].sexo, "feminino")!=0){
             printf("entrada invalida!");
-            exit(1);     
+            exit(1);
         }
     }
      maiAltura(vPessoa, &maiorAlt);
      menAltura(vPessoa, &menorAlt);
      mediaAltMulheres(vPessoa, nroMulheres, &mediaAltFem);
      printf("Maior altura %.2f, menor altura %.2f, media de altura das mulheres %.2f, numero de homens %d", maiorAlt, menorAlt, mediaAltFem, nroHomens);
-   
+
  }
